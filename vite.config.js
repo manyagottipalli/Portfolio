84d36cc0da5c69847240076b256be4130e8c8091
Manya <manyagottipalli1273@gmail.com>
2025-03-24 18:06:36 +0530

    second commit

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    port: process.env.PORT || 3000, // Use Render's port or default to 3000
+    host: '0.0.0.0', // Ensure it binds to all IPs
+  }
 });
